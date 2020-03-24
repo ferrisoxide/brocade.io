d66c927cd26998c3e0bd84a830dc6caccf084336
Tom Tuddenham <ferrisoxide@gmail.com>
2020-03-24 22:48:43 +1030

    fix devise mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,5 +112,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Required by Devise
-  config.action_mailer.default_url_options = { host: 'brocade.io' }
+  config.action_mailer.default_url_options = { host: 'www.brocade.io' }
 end
