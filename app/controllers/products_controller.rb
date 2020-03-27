6106a52c8bc97abfe4fb999f4bf0302c8513a575
Tom Tuddenham <ferrisoxide@gmail.com>
2020-03-27 20:39:05 +1030

    minor fixes to log in / sign up page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
 
   include ProductSearch
 
-  before_action :set_product, only: [:show, :update, :destroy]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
     @products = product_search(params)
@@ -11,10 +11,22 @@ class ProductsController < ApplicationController
   def show
   end
 
+  def edit
+
+  end
+
+  def update
+    @product.update(product_params)
+  end
+
   private
 
   def set_product
     @product = Product.from_param(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit()
   end
 
 end
