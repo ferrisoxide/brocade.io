d4a352b7343c268b38bef7d9fa547d0111d69794
Tom Tuddenham <ferrisoxide@gmail.com>
2024-12-12 23:59:54 +1030

    out-of-band disabled throttling

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
-Rack::Attack.throttle('requests by ip', limit: 5, period: 60, &:ip)
+# Enable throttling if the THROTTLE environment variable is set to literal 'true' or '1'
+Rack::Attack.throttle('requests by ip', limit: 5, period: 60, &:ip) if ENV['THROTTLE'].in?(%w[true 1])
